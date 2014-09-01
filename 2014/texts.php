de75eea1484b91edd44acc4be879718fd0767af8
Enes Huseinčehajić <bezbeli@gmail.com>
2014-09-01 18:11:52 +0200

    Added translations to text.php

--- 2014/texts.php
+++ 2014/texts.php
@@ -8,8 +8,8 @@ $text = array(
   'lang' => 'bs',
   //meta
   'author' => 'ZastoNe.ba',
-  'description' => 'Eurovoľby 2014 sa blížia! Spočítajte si koho voliť!',
-  'keywords' => 'voľby 2014, európske voľby, volebná kalkulačka, volebný test',
+  'description' => 'Opšti/Opći izbori 2014. u BiH se približavaju! Glasometar će vam pomoći da saznate sa kojom partijom se najviše slažete!',
+  'keywords' => 'Izbori 2014, Izbori BiH, Glasometar, glasometar kviz',
   //fb
   'og:image' => 'http://volebnakalkulacka.sk/image/logo.jpg',
   //'og:title' => 'Volební kalkulačka',
@@ -19,7 +19,7 @@ $text = array(
   'licence' => 'Creative Commons BY 4.0',
   'year' => '2014',
   
-  'footer_text' => '<a href="http://glasometar.ba">Glasometar.ba</a> <br/>Ponosni član <a href="http://www.votematch.eu/">VoteMatchEurope</a>',
+  'footer_text' => '<a href="http://glasometar.ba">Glasometar.ba</a> <br/> <a href="http://www.votematch.eu/">VoteMatchEurope</a>',
   
 
 
@@ -32,10 +32,10 @@ $text = array(
   'calc_name' => 'Glasometar 2014',
   'page_question_yes' => 'DA',
   'page_question_no' => 'NE',
-  'page_question_neutral' => 'ne znam',
-  'page_question_neutral_add' => ' / ne znanima me',
+  'page_question_neutral' => 'Ne znam',
+  'page_question_neutral_add' => ' / 	Ne znanima me',
   'page_important_questions' => 'Važna pitanja', 
-  'page_important_legend' => '<strong>Označite pitanja koja su najvažnija za vas</strong>. Podrobnije odredite rezultat. Ovaj korak nije obavezan i možete ga preskočiti.',
+  'page_important_legend' => '<strong>Označite pitanja koja su najvažnija za vas</strong>. Ovaj korak nije obavezan i možete ga preskočiti.',
   'page_important_skip' => 'Preskoči',
   'page_important_results' => 'Rezultati',  
   
@@ -48,38 +48,38 @@ $text = array(
    'result_winner_share' => 'Podijelite rezultate na Facebook',    
    'result_share' => 'Podijelite rezultate',
    
-   'comparison_parties_eu' => 'Poređenje sa strankama u EU',
-   'comparison_eu_explained' => '15 issues that are in the Slovak election calculator, are also in electoral calculators in other EU countries. Which side are you there next?',
-   'missing_table_description' => 'The following pages <strong> questions not answered yet </strong>: ',
+   //'comparison_parties_eu' => 'Poređenje sa strankama u EU',
+   //'comparison_eu_explained' => '15 issues that are in the Slovak election calculator, are also in electoral calculators in other EU countries. Which side are you there next?',
+   'missing_table_description' => 'Za ove stranice <strong> pitanja nisu odgovorena </strong>: ',
    
    //fb
    'result_fb_appid' => "647475721940594",
   'result_fb_picture_link' => 'http://volebnakalkulacka.sk/europsky-parlament-2014/image/logo/',
-  'result_fb_name' => 'Vo Volebné kalkulačke mám najvyššiu zhodu:',
-  'result_fb_description_1' => 'Podľa Volebné kalkulačky pre eurovoľby sa so mnou najviac zhodujú: ',
-  'result_fb_description_match' => 'zhoda',
-  'result_fb_description_and' => 'a',
-  'result_fb_description_only' => 'len',
-  'result_fb_description_2' => 'Najmenej ',
-  'result_fb_description_3' => 'S kým sa najviac zhodujete na VolebnaKalkulacka.sk',
-  'result_fb_redirect_link' => 'http://volebnakalkulacka.sk/',
-  'result_fb_link' => 'http://volebnakalkulacka.sk/',
+  'result_fb_name' => '[Glasometar.ba] Moji stavovi se najviše podudaraju sa ovim partijama:',
+  'result_fb_description_1' => '[Glasometar.ba] Moji stavovi se najviše podudaraju sa ovim partijama: ',
+  'result_fb_description_match' => 'slaganje',
+  'result_fb_description_and' => 'i',
+  'result_fb_description_only' => 'samo',
+  'result_fb_description_2' => 'Najmanje ',
+  'result_fb_description_3' => 'Sa kojom partijom se najvise slažete - Glasometar.ba',
+  'result_fb_redirect_link' => 'http://glasometar.ba',
+  'result_fb_link' => 'http://glasometar.ba/',
   'result_fb_action' => "[{'name':'Koho voliť?','link':'http://volebnakalkulacka.sk/'}]",   
    
    //tabs
    'result_results' => 'Rezultati',
-   'result_comparison' => 'Usporedba',
-   'result_eu' => 'Rezultati u EU',
+   'result_comparison' => 'Poređenje',
+   //'result_eu' => 'Rezultati u EU',
    
    //support (DMS)
-   'result_support' => '<strong>Podpořte tvorbu nezávislých
-  Volebních kalkulaček.</strong><br/>
-  Pošlete SMS na&nbsp;číslo&nbsp;<strong><span class="colorstrong">87777</span></strong>
-  s&nbsp;textem <strong><span class="colorstrong">DMS&nbsp;KOHOVOLIT</span></strong>
-  (cena 30Kč, z toho 28Kč pro
-   sdružení KohoVolit.eu) nebo 
-   <strong><span class="colorstrong">DMS&nbsp;ROK&nbsp;KOHOVOLIT</span></strong>
-   (automaticky každý měsíc po 1 rok)',
+   //'result_support' => '<strong>Podpořte tvorbu nezávislých
+  //Volebních kalkulaček.</strong><br/>
+  //Pošlete SMS na&nbsp;číslo&nbsp;<strong><span class="colorstrong">87777</span></strong>
+  //s&nbsp;textem <strong><span class="colorstrong">DMS&nbsp;KOHOVOLIT</span></strong>
+  //(cena 30Kč, z toho 28Kč pro
+  // sdružení KohoVolit.eu) nebo 
+   //<strong><span class="colorstrong">DMS&nbsp;ROK&nbsp;KOHOVOLIT</span></strong>
+   //(automaticky každý měsíc po 1 rok)',
    
    //comparison
    'result_close' => 'Zatvori',
@@ -110,14 +110,14 @@ $text = array(
   
   //RESULTS
 
-  'result_twitter_data_lang' => 'sk',
-  'result_twitter_text_1' => 'Euro-volebná kalkulačka:1.',
+  'result_twitter_data_lang' => 'bs',
+  'result_twitter_text_1' => '[Glasometar.ba] moji rezultati:1.',
   //'result_twitter_text_2' => '1.',
   //'result_twitter_and' => 'a',
-  'result_twitter_text_3' => ', naopak ',
-  'result_twitter_account' => 'KohoVolitEU',
-  'result_twitter_hashtags' => 'volby,volby2014,eurovolby',
-  'result_twitter_share' => 'Tweetni',
+  //'result_twitter_text_3' => ', naopak ',
+  //'result_twitter_account' => 'KohoVolitEU',
+  //'result_twitter_hashtags' => 'volby,volby2014,eurovolby',
+  //'result_twitter_share' => 'Tweetni',
 
   
   
